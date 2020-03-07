61350b6705fb45adeb98cb88c01c819ce3751d85
decaby7e <27081624+decaby7e@users.noreply.github.com>
2020-03-06 23:21:27 -0500

    Update animations.js

--- src/animations.js
+++ src/animations.js
@@ -1,20 +1,24 @@
-/* Global Variables */
+* Global Variables */
 var speed = 100;
 
-
-/* Create navigation option with blinking cursor */
-function typeWriter(txt, id, i=0) {
+/* Type out text */
+function type(txt, id, i=0) {
   if (i < txt.length) {
     document.getElementById(id).innerHTML = txt.substring(0, i + 1) + "|";
     i++;
-    setTimeout(typeWriter, speed, txt, id, i);
+    setTimeout(type, speed, txt, id, i);
   }
-
   else {
-    blinkCursor(id);
+    document.getElementById(id).innerHTML = txt.substring(0, txt.length);
   }
 }
 
+/* Type out text with a blinking cursor */
+function typeWriter(txt, id, i=0) {
+  type(txt, id, i);
+
+  blinkCursor(id);
+}
 
 /* Add a blinking cursor to the end of an element */
 function blinkCursor(id) {
